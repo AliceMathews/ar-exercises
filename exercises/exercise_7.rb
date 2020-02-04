11a9e1da8b82c75dc44aa1fd76f7bfe1dfd2ed12
AliceMathews <alice_mathews@hotmail.co.uk>
2020-02-04 01:33:37 +0000

    added validation

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,15 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+# @store1 = Store.find_by(id: 1)
+
+# @store1.employees.create(first_name: "ghfda", last_name: "Virani", hourly_rate: 55)
+
+# employee = Employee.create(first_name: "Alice", last_name: "Virani", hourly_rate: 55 )
+
+
+print "Give me a store name: "
+@store7 = gets.chomp
+store = Store.create(name: @store7)
+puts store.errors.messages
+
--- lib/store.rb
+++ lib/store.rb
@@ -1,4 +1,17 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  
+  validates :name, length: {minimum: 3}
+  validates :annual_revenue, numericality: {only_integer: true, greater_than: 0}
+
+  validate :must_carry_mens_or_womens_apparel
+
+  def must_carry_mens_or_womens_apparel
+    if (!mens_apparel && !womens_apparel)
+      errors.add(:apparel, "must sell something")
+    end
+  end
+
 
 end
+
